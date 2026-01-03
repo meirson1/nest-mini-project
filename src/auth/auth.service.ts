a47b4a266c1cdb97c88888df9426fd9de877bf5b
Noam Meir <noam220599@gmail.com>
2026-01-03 16:05:41 +0200

    Add logic to signin and signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { message: 'I am signup endpoint' };
+  async signup(dto: AuthDto) {
+    //Generate the password hash
+    const hash = await argon.hash(dto.password);
+    //Save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      //return the saved user
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { hash: _hash, ...userWithoutHash } = user;
+      return userWithoutHash;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  signin() {
-    return { message: 'I am signin endpoint' };
+  async signin(dto: AuthDto) {
+    //find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Certedentials incorrect');
+    //compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    //if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+    //send back the user
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { hash: _hash, ...userWithoutHash } = user;
+    return userWithoutHash;
   }
 }
