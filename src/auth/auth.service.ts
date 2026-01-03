03f36c5f8595c7ff68de3812ad3269be2169dfb2
Noam Meir <noam220599@gmail.com>
2026-01-03 18:51:30 +0200

    Add JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     //Generate the password hash
     const hash = await argon.hash(dto.password);
@@ -20,9 +26,7 @@ export class AuthService {
       });
 
       //return the saved user
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { hash: _hash, ...userWithoutHash } = user;
-      return userWithoutHash;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -47,8 +51,29 @@ export class AuthService {
     //if password incorrect throw exception
     if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
     //send back the user
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { hash: _hash, ...userWithoutHash } = user;
-    return userWithoutHash;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private config: ConfigService) {
+    const secret = config.get<string>('JWT_SECRET');
+    if (!secret) {
+      throw new Error('JWT_SECRET is not set');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: secret,
+    });
+  }
+
+  validate(payload: { sub: number; email: string }) {
+    return payload;
+  }
+}
